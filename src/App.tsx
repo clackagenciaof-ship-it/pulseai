7a3e0287337a024c67eb3280b9d6ab9d42384e22
clackagenciaof-ship-it <clackagenciaof@gmail.com>
2026-07-20 17:04:20 -0300

    feat: adicionar roteamento real por perfil

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
 import './ui.css'
 import './polish.css'
 import AppPlus from './AppPlus'
+import AuthPortal from './AuthPortal'
+import { AdminArea, BusinessArea, DriverArea, UserArea } from './Areas'
 
-export default AppPlus
+export default function App() {
+  const path = window.location.pathname.replace(/\/+$/, '') || '/'
+
+  if (path === '/entrar' || path === '/cadastro') return <AuthPortal />
+  if (path === '/cadastro/negocio') return <AuthPortal forcedRole="business" />
+  if (path === '/cadastro/motorista') return <AuthPortal forcedRole="driver" />
+  if (path === '/app') return <UserArea />
+  if (path === '/parceiro') return <BusinessArea />
+  if (path === '/motorista') return <DriverArea />
+  if (path === '/admin') return <AdminArea />
+
+  return <AppPlus />
+}
